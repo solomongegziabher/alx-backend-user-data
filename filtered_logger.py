0225f40c6c4d5601298fc8c54a6f81689fdd06c3
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-09-01 21:16:48 +0300

    Update filtered_logger.py

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,13 +1,71 @@
 #!/usr/bin/env python3
-"""log message obfuscated"""
+"""
+Module for handling Personal Data
+"""
 from typing import List
 import re
-import os
 import logging
+from os import environ
 import mysql.connector
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+def main():
+    """
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
 
 
 class RedactingFormatter(logging.Formatter):
@@ -19,55 +77,15 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """initializer"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """filter values in incoming log records using"""
-        message = filter_datum(self.fields, self.REDACTION,
-                               super(RedactingFormatter, self).format(record),
-                               self.SEPARATOR)
-        return message
+        """ Filters values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
-def get_logger() -> logging.Logger:
-    """returns a logging.Logger object"""
-    logging.getLogger('user_data').setLevel(logging.INFO)
-    logging.getLogger('user_data').propagate = False
-    logging.getLogger('user_data').addHandler(logging.StreamHandler())
-    logging.StreamHandler().setFormatter(RedactingFormatter(PII_FIELDS))
-    return logging.getLogger('user_data')
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """returns a connector to the database"""
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    cnx = mysql.connector.connection.MySQLConnection(
-        user=username, password=password, host=host, database=db_name)
-    return cnx
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(field + "=.*?" + separator,
-                         field + "=" + redaction + separator, message)
-    return message
-
-
-def main():
-    """main function that takes no arguments and returns nothing"""
-    cnx = get_db()
-    cnx.cursor().execute("SELECT * FROM users;")
-    fields = [i[0] for i in cnx.cursor().description]
-
-    for row in cnx.cursor():
-        row_ = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, fields))
-        get_logger().info(row_.strip())
-    cnx.cursor().close()
-    cnx.close()
+if __name__ == '__main__':
+    main()
